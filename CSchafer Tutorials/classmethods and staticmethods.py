904a30c35932d4cebcdbe4576a8dcfd8fe5cd567
jmoro0408 <james.moro.11@gmail.com>
2021-10-23 14:36:16 -0700

    class methods and static methods

--- CSchafer Tutorials/classmethods and staticmethods.py
+++ CSchafer Tutorials/classmethods and staticmethods.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Employee:
 
     raise_amount = 1.04
@@ -17,6 +20,29 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)  # 4% raise
 
+    @classmethod
+    def set_raise_amount(cls, amount):
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):  # alternative constructors start from_
+        first, last, pay = emp_str.split("-")
+        return cls(
+            first, last, pay
+        )  # creates a new employee object from string. This is an alternative constructor
+
+    @staticmethod  # static methods are logically linked to the class but doesnt access the class or instance
+    def is_workday(day):
+        if day.weekday() > 4:  # Mon = 0, Sat/Sun = 5/6.
+            return False
+        return True
+
 
 emp_1 = Employee("James", "Moro", 50000)
 emp_2 = Employee("test", "name", 60000)
+
+Employee.set_raise_amount(1.05)  # can use class methods to overwrite class variables
+
+my_date = datetime.date(2021, 10, 23)  # Saturday
+
+print(Employee.is_workday(my_date))
