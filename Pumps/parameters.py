7222c77ea74191cb27c56c631b757b3b1d221b10
jmoro0408 <james.moro.11@gmail.com>
2021-10-24 16:58:53 -0700

    added parse_xylect_curve function

--- Pumps/parameters.py
+++ Pumps/parameters.py
@@ -1,4 +1,8 @@
 class Pump:
+
+    flow = None
+    head = None
+
     def __init__(self, make, model, speed):
         self.make = make
         self.model = model
@@ -15,9 +19,9 @@ class Pump:
         """
         return self.make + " " + self.model
 
-    # def define_pumpcurve(self, flow_head_dict: dict):
+    def define_pumpcurve(self, flow_head_dict: dict):
+        self.flow = flow_head_dict.keys()
+        self.head = flow_head_dict.values()
 
 
 N3312 = Pump("Xylem", "N3312", 100)
-
-print(N3312.fullname())
---
+++ Pumps/parse_curve.py
@@ -0,0 +1,55 @@
+import pandas as pd
+from pathlib import Path
+import json
+
+PUMP_CURVE_FILEPATH = (
+    r"/Users/jamesmoro/Documents/Python/OOP_Python/Pumps/pump_curves.xls"
+)
+
+
+def parse_xylect_curve(pump_curve_filepath: str):
+    """Function parse the output of a xylect pump curve into a dictionary.
+    This function expects the excel file to be in the standard Xylect output format.
+    It first grabs the first three rows of the file, which are typically information about
+    the specific pump i.e model, motor, and impeller info and stores them in a dict.
+    It then deletes these rows and convert the rest of the available info into a dict, which
+    is combined with the pump info dict.
+
+    Args:
+        pump_curve_filepath (str): location of the exported xylect curve (.xls)
+
+    Returns:
+        dictionary: a dictionary containing all the information from the xylect .xls
+    """
+    _pump_curve = pd.read_excel(PUMP_CURVE_FILEPATH)
+    _pump_info_dict = {
+        "Pump": _pump_curve["Unnamed: 1"][0],
+        "Motor": _pump_curve["Unnamed: 1"][1],
+        "Impeller": _pump_curve["Unnamed: 1"][2],
+    }
+    _pump_curve = _pump_curve.iloc[4:, :]  # dropping the info already retrieved
+    _pump_curve = _pump_curve.rename(columns=_pump_curve.iloc[0]).drop(
+        _pump_curve.index[0]
+    )  # swapping the header names with the first row
+
+    _pump_curve_dict = _pump_curve.to_dict(
+        orient="list"
+    )  # converting df to dict with strutcture {column: [values]}
+    pump_dict = {**_pump_info_dict, **_pump_curve_dict}  # combining the two dicts
+
+    del _pump_curve, _pump_info_dict, _pump_curve_dict  # deleting unused data
+
+    return pump_dict
+
+
+def write_json(pump_dict: dict):
+    cwd = Path.cwd()
+    save_path = Path(cwd / "data.json")
+    with open(save_path, "w") as fp:
+        json.dump(pump_dict, fp, indent=4)
+    print(f"Pump curve data saved as data.json in {cwd} ")
+
+
+if __name__ == "__main__":
+    pump_curve = parse_xylect_curve(PUMP_CURVE_FILEPATH)
+    write_json(pump_curve)
